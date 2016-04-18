b38665ba7cfb874cce817548c1a55b6eff4eb0cd
helenxiong12 <helenxiong12@gmail.com>
2016-04-19 00:29:29 +0900

    does htis wokr

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -1,15 +1,11 @@
 package com.flatironschool.javacs;
 
 public class HelloWorld {
-    public static /*Double*/String getVersion() {    
-    	/*String version = System.getProperty("java.version");
-    	int pos = version.indexOf('.');
-    	pos = version.indexOf('.', pos+1);
-    	return Double.parseDouble(version.substring(0, pos);//);*/
-    	return System.getProperty("java.version");
+    public static Double getVersion() {    
+    	return Double.parseDouble(System.getProperty("java.specification.version"));
     }
 
     public static void main(String[] args) {
-		System.out.println(getVersion());
+
     }
 }
